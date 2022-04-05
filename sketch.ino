1d3a7f8af52fd240d8ed9baef027a7762f0d9eac
unknown <luzharck@gmail.com>
2022-04-05 17:59:38 -0500

    testing git

--- sketch.ino
+++ sketch.ino
@@ -4,13 +4,9 @@ Game* game;
 void setup() {
   Serial.begin(9600);
   randomSeed(analogRead(0));
-  
   game = new Game();
   game->start();
 }
 
 void loop(){
-  // print a random number from 10 to 19
-
-
 }
